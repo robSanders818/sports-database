4db0fc7dd2d9a2c7183ca7bb05251e52ece80927
Bolaji <rotibi.m@husky.neu.edu>
2019-06-20 20:50:56 -0400

    Finished Site
    All of the main functionalities of this site have been implemented. Bug fixing is needed.

--- routes/playerController.js
+++ routes/playerController.js
@@ -16,12 +16,12 @@ router.use('/components', express.static('components'));
 
 
 // Getting the string of the webpage
-router.get('/:playerid', (req, res) => {
+router.get('/pitcher/:playerid', (req, res) => {
     playerid = req.params.playerid; // The Review JSON Object
 
     // Finds the user matching the id of the post
 
-    pool.query('CALL player_stats(?)', playerid, (error, results, fields) => {
+    pool.query('CALL pitching_stats(?)', playerid, (error, results, fields) => {
 
 
         //if theres an error it redirects the user to the dashboard(for now)
@@ -36,9 +36,38 @@ router.get('/:playerid', (req, res) => {
             pool.query('CALL get_player_name(?)', playerid, (error, results, fields) => {
                     console.log(results[0].length)
 
-                        res.render('player.ejs', {
+                        res.render('pitcher.ejs', {
                             playerStats: playerStats,
-                            fields: fields[0],
+                            playerName: results[0]
+                        })
+                    })
+                    }
+});
+})
+
+// Getting the string of the webpage
+router.get('/batter/:playerid', (req, res) => {
+    playerid = req.params.playerid; // The Review JSON Object
+
+    // Finds the user matching the id of the post
+
+    pool.query('CALL batter_stats(?)', playerid, (error, results, fields) => {
+
+
+        //if theres an error it redirects the user to the dashboard(for now)
+        if (error) res.redirect('/dashboard');
+
+        //if there are no posts it redirects the user to the dashboard(for now)
+        if (results == null) {
+            res.redirect('/dashboard');
+        } else {
+            var playerStats = results[0];
+
+            pool.query('CALL get_player_name(?)', playerid, (error, results, fields) => {
+                    console.log(results[0].length)
+
+                        res.render('batter.ejs', {
+                            playerStats: playerStats,
                             playerName: results[0]
                         })
                     })
